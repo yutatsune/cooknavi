7297796d6e6408eb12da8f8de391051939e52ad6
yutatsune <66116053+yutatsune@users.noreply.github.com>
2020-11-21 22:48:10 +0900

    relationships_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :materials
   has_many :recipe_likes
   has_many :liked_recipes, through: :recipe_likes, source: :recipe
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_rerationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_rerationships, source: :user
   def already_liked?(recipe)
     self.recipe_likes.exists?(recipe_id: recipe.id)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_120236) do
+ActiveRecord::Schema.define(version: 2020_11_21_132052) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -73,6 +73,16 @@ ActiveRecord::Schema.define(version: 2020_11_14_120236) do
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "follow_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -94,4 +104,6 @@ ActiveRecord::Schema.define(version: 2020_11_14_120236) do
   add_foreign_key "recipe_likes", "recipes"
   add_foreign_key "recipe_likes", "users"
   add_foreign_key "recipes", "users"
+  add_foreign_key "relationships", "users"
+  add_foreign_key "relationships", "users", column: "follow_id"
 end
