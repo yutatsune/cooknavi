0d0d94d67dd2d5ee71786e73d8976eb48a4cbca2
yutatsune <66116053+yutatsune@users.noreply.github.com>
2020-12-26 21:17:13 +0900

    JavaScript

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,58 +1,89 @@
 $( ()=> {
-  // 画像用のinputを生成する関数
-  const buildFileField = (num)=> {
-    const html = `<div data-index="${num}" class="js-file_group">
-                    <input class="js-file" type="file"
-                    name="recipe[images_attributes][${num}][src]"
-                    id="recipe_images_attributes_${num}_src"><br>
-                    <div class="js-remove">削除</div>
-                  </div>`;
+  //共通の定数を定義==================================================================
+  const prevContent = $('.label-content').prev();
+
+  //プレビューのhtmlを定義============================================================
+  function buildHTML(id,image) {
+    var html = `<div class="preview-box">
+                  <div class="upper-box">
+                    <img src=${image} alt="preview">
+                  </div>
+                  <div class="lower-box">
+                    <div class="update-box">
+                      <div class="edit-btn">編集</div>
+                    </div>
+                    <div class="delete-box">
+                      <div class="delete-btn" data-delete-id= ${id}>削除</div>
+                    </div>
+                  </div>
+                </div>`
     return html;
   }
-  // プレビュー用のimgタグを生成する関数
-  const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
-    return html;
+  //ラベルのwidth・id・forの値を変更==================================================
+  function setLabel(count) {
+    //プレビューが5個あったらラベルを隠す
+    if (count == 5) { 
+      $('.label-content').hide();
+    } else {
+      //プレビューが4個以下の場合はラベルを表示
+      $('.label-content').show();
+      //プレビューボックスのwidthを取得し、maxから引くことでラベルのwidthを決定
+      labelWidth = (620 - parseInt($(prevContent).css('width')));
+      $('.label-content').css('width', labelWidth);
+      //id・forの値を変更
+      $('.label-box').attr({for: `recipe_images_attributes_${count}_src`});
+    }
   }
 
-  // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-  // 既に使われているindexを除外
-  lastIndex = $('.js-file_group:last').data('index');
-  fileIndex.splice(0, lastIndex);
+  //編集ページ(recipes/:i/edit)へリンクした際のアクション==================================
+  if (window.location.href.match(/\/recipes\/\d+\/edit/)){
+    //プレビューの数を取得
+    var count = $('.preview-box').length;
+    //countに応じてラベルのwidth・id・forの値を変更
+    setLabel(count) 
+  }
 
-  $('.hidden-destroy').hide();
-
-  $('#image-box').on('change', '.js-file', function(e) {
-    const targetIndex = $(this).parent().data('index');
-    // ファイルのブラウザ上でのURLを取得する
-    const file = e.target.files[0];
-    const blobUrl = window.URL.createObjectURL(file);
-
-    // 該当indexを持つimgがあれば取得して変数imgに入れる(画像変更の処理)
-    if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
-      img.setAttribute('src', blobUrl);
-    } else {  // 新規画像追加の処理
-      $('#previews').append(buildImg(targetIndex, blobUrl));
-      // fileIndexの先頭の数字を使ってinputを作る
-      $('#image-box').append(buildFileField(fileIndex[0]));
-      fileIndex.shift();
-      // 末尾の数に1足した数を追加する
-      fileIndex.push(fileIndex[fileIndex.length - 1] + 1);
+  //プレビューの追加=================================================================
+  $(document).on('change', '.hidden-field', function() {
+    //hidden-fieldのidの数値のみ取得
+    var id = $(this).attr('id').replace(/[^0-9]/g, '');
+    //選択したfileのオブジェクトを取得
+    var file = this.files[0];
+    var reader = new FileReader();
+    //readAsDataURLで指定したFileオブジェクトを読み込む
+    reader.readAsDataURL(file);
+    //読み込み時に発火するイベント
+    reader.onload = function() {
+      var image = this.result;
+      //htmlを作成
+      var html = buildHTML(id,image);
+      //ラベルの直前のプレビュー群にプレビューを追加
+      $(prevContent).append(html);
+      //プレビュー削除したフィールドにチェックボックスがあった場合、チェックを外す
+      if ($(`#recipe_images_attributes_${id}__destroy`)){
+        $(`#recipe_images_attributes_${id}__destroy`).prop('checked',false);
+      } 
+      //プレビューの数を取得
+      var count = $('.preview-box').length;
+      //countに応じてラベルのwidth・id・forの値を変更
+      setLabel(count);
     }
   });
 
-  $('#image-box').on('click', '.js-remove', function() {
-    const targetIndex = $(this).parent().data('index');
-    // 該当indexを振られているチェックボックスを取得する
-    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-    // もしチェックボックスが存在すればチェックを入れる
-    if (hiddenCheck) hiddenCheck.prop('checked', true);
-
-    $(this).parent().remove();
-    $(`img[data-index="${targetIndex}"]`).remove();
-
-    // 画像入力欄が0個にならないようにしておく
-    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+  // 画像の削除=====================================================================
+  $(document).on('click', '.delete-btn', function() {
+    var id = $(this).attr('data-delete-id')
+    //削除用チェックボックスがある場合はチェックボックスにチェックを入れる
+    if ($(`#recipe_images_attributes_${id}__destroy`).length) {
+      $(`#recipe_images_attributes_${id}__destroy`).prop('checked',true);
+    }
+    //画像を消去
+    $(this).parent().parent().parent().remove();
+    //フォームの中身を削除
+    $(`#recipe_images_attributes_${id}_src`).val("");
+    //プレビューの数を取得
+    var count = $('.preview-box').length;
+    //countに応じてラベルのwidth・id・forの値を変更
+    setLabel(count);
   });
 });
