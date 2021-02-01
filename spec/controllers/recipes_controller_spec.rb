8153b32e04d33305380524cff3ab5e72ffed5d96
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-02-01 22:22:48 +0900

    test_update

---
+++ spec/controllers/recipes_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe RecipesController do
+  before do
+    user = create(:user)
+    login user
+  end
+  describe "#index" do
+    it "created_at DESCで並べ替えられたツイートの配列を入力できるか" do
+      recipes = create_list(:recipe, 3)
+      get :index
+      expect(assigns(:recipes)).to match(recipes.sort{|a, b| b.created_at <=> a.created_at })
+    end
+    it "indexテンプレートをレンダリングできるか" do
+      get :index
+      expect(response).to render_template :index
+    end
+    it "200レスポンスが返ってきているか" do
+      get :index
+      expect(response).to have_http_status "200"
+    end
+  end
+  describe 'GET #new' do
+    it "newテンプレートをレンダリングできるか" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+  describe 'GET #edit' do
+    it "リクエストされたrecipeを@recipeに割り当てられるか" do
+      recipe = create(:recipe)
+      get :edit, params: { id: recipe }
+      expect(assigns(:recipe)).to eq recipe
+    end
+
+    it "editテンプレートをレンダリングできるか" do
+      recipe = create(:recipe)
+      get :edit, params: { id: recipe }
+      expect(response).to render_template :edit
+    end
+  end
+end
