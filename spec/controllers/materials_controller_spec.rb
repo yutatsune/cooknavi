c8ae8af2e51868e4f677985b5eb659a1c6b1086d
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-02-02 23:24:50 +0900

    test_update

--- spec/controllers/materials_controller_spec.rb
+++ spec/controllers/materials_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-describe RecipesController do
+describe MaterialsController do
   describe "#index" do
     it "created_at DESCで並べ替えられたツイートの配列を入力できるか" do
-      recipes = create_list(:recipe, 3)
+      materials = create_list(:material, 3)
       get :index
-      expect(assigns(:recipes)).to match(recipes.sort{|a, b| b.created_at <=> a.created_at })
+      expect(assigns(:materials)).to match(materials.sort{|a, b| b.created_at <=> a.created_at })
     end
     it "indexテンプレートをレンダリングできるか" do
       get :index
@@ -30,15 +30,15 @@ describe RecipesController do
     context "no login user" do
       it "indexにリダイレクトするかどうか" do
         get :new
-        expect(response).to redirect_to(recipes_path)
+        expect(response).to redirect_to(materials_path)
       end
     end
   end
   describe 'GET #edit' do
-    it "リクエストされたrecipeを@recipeに割り当てられるか" do
-      recipe = create(:recipe)
-      get :edit, params: { id: recipe }
-      expect(assigns(:recipe)).to eq recipe
+    it "リクエストされたmaterialを@materialに割り当てられるか" do
+      material = create(:material)
+      get :edit, params: { id: material }
+      expect(assigns(:material)).to eq material
     end
     context "login user" do
       before do
@@ -46,8 +46,8 @@ describe RecipesController do
         login user
       end
       it "editテンプレートをレンダリングできるか" do
-        recipe = create(:recipe)
-        get :edit, params: { id: recipe }
+        material = create(:material)
+        get :edit, params: { id: material }
         expect(response).to render_template :edit
       end
     end
@@ -55,10 +55,10 @@ describe RecipesController do
       it "indexにリダイレクトするかどうか" do
         user = create(:user)
         login user
-        recipe = create(:recipe)
+        material = create(:material)
         logout user
-        get :edit, params: { id: recipe }
-        expect(response).to redirect_to(recipes_path)
+        get :edit, params: { id: material }
+        expect(response).to redirect_to(materials_path)
       end
     end
   end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -3,4 +3,8 @@ module ControllerMacros
     @request.env["devise.mapping"] = Devise.mappings[:user]
     sign_in user
   end
+  def logout(user)
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_out user
+  end
 end
