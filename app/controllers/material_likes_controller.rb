20d0199a419158629c315bccbe8276725465e074
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-04-05 23:58:16 +0900

    material_like

--- app/controllers/material_likes_controller.rb
+++ app/controllers/material_likes_controller.rb
@@ -1,14 +1,21 @@
 class MaterialLikesController < ApplicationController
+
+  before_action :set_material
+
   def create
-    @like = current_user.material_likes.create(material_id: params[:material_id])
-    flash[:notice] = '投稿に｢いいね！｣しました'
-    redirect_to material_path(params[:material_id])
+    @like = current_user.material_likes.new(material_id: @material.id)
+    @like.save
+    @likes = MaterialLike.where(material_id: @material.id)
   end
 
   def destroy
-    @like = MaterialLike.find_by(material_id: params[:material_id], user_id: current_user.id)
-    @like.destroy
-    flash[:notice] = '｢いいね！｣を解除しました'
-    redirect_to material_path(params[:material_id])
+    @like = MaterialLike.find_by(material_id: @material.id, user_id: current_user.id).destroy
+    @likes = MaterialLike.where(material_id: @material.id)
+  end
+
+  private
+
+  def set_material
+    @material = Material.find(params[:material_id])
   end
 end
